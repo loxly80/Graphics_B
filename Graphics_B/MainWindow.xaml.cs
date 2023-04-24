5f0a2c29e46e43508c02ed52e05b5f38599e4464
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-04-24 15:30:54 +0200

    Add project files.

---
+++ Graphics_B/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Graphics_B
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    List<Point> points = new List<Point>();
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      if(e.LeftButton == MouseButtonState.Pressed)
+      {
+        var location = e.GetPosition(ActiveCanvas);
+        Point point = new Point(location.X, location.Y);
+        points.Add(point);
+        point.AddToCanvas(ActiveCanvas);
+      }
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+      string json = JsonConvert 
+    }
+  }
+}
---
+++ Graphics_B/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Graphics_B
+{
+  public class Point
+  {
+    public double X { get; }
+    public double Y { get; }
+
+    public Point(double x, double y)
+    {
+      X = x;
+      Y = y;
+    }
+
+    public void AddToCanvas(Canvas canvas)
+    {
+      Ellipse ellipse = new Ellipse();
+      ellipse.Width = 8;
+      ellipse.Height = 8;
+      ellipse.Fill = Brushes.Black;
+      Canvas.SetLeft(ellipse,X - 4);
+      Canvas.SetTop(ellipse,Y - 4);
+      canvas.Children.Add(ellipse);
+    }
+  }
+}
